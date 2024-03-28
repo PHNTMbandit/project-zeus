ded9395e3b29617eb19f6d7ff6dc8762b8aaed23
Domenic Pittari <dom.pittari@gmail.com>
2024-03-28 14:48:43 +0900

    Fixed CORS policy

--- src/api/weather-api.ts
+++ src/api/weather-api.ts
@@ -3,7 +3,7 @@ import { City } from "@/types/city";
 export const getCity = async (cityPrefix: string) => {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${cityPrefix}&limit=5&appid=beee9aed34af7b7e2e938db81d68bb9e`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${cityPrefix}&limit=5&appid=beee9aed34af7b7e2e938db81d68bb9e`
     );
     const data = await response.json();
     return data;
